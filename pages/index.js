670d9abe512ed22ebaf48ab6ce226fa1726497f9
Cooper Saye <coopersaye@gmail.com>
2021-11-14 13:03:36 -0800

    Create form section.

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,24 @@ export default function Index() {
             <h1>Wickfaces</h1>
             <p>Connect with classmates and stay in touch after graduation.</p>
           </div>
+          <div className={styles.form}>
+            <form onSubmit={e => {
+              e.preventDefault();
+              register();
+            }}>
+              <input
+                placeholder="School email"
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                autoComplete="email"
+                required
+              />
+              <button>Register</button>
+            </form>
+            <p>You must have a Chadwick email to register.</p>
+            <p>This website is not affiliated with Chadwick School.</p>
+          </div>
         </div>
       </div>
       <div className={styles.footer}>
