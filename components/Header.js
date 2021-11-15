b0306aabe040702527fd46382468c4f9c0218b61
Cooper Saye <coopersaye@gmail.com>
2021-11-15 08:07:02 -0800

    Clean up header.

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Tooltip from '@mui/material/Tooltip';
+
+import { getAuth, signOut } from 'firebase/auth';
 
 import styles from '../styles/components/Header.module.css';
 
 export default function Header() {
+  const auth = getAuth();
+
   return (
     <div className={styles.container}>
       <Link href="/">
@@ -18,15 +24,25 @@ export default function Header() {
         </a>
       </Link>
       <span className="flexfill" />
-      <Link href="/profile">
-        <a className={styles.link}>Profile</a>
-      </Link>
-      <Link href="/board">
-        <a className={styles.link}>Board</a>
-      </Link>
-      <Link href="/messages">
-        <a className={styles.link}>Messages</a>
-      </Link>
+      {
+        auth.currentUser &&
+        <>
+          <Link href="/profile">
+            <a className={styles.link}>Profile</a>
+          </Link>
+          <Link href="/board">
+            <a className={styles.link}>Board</a>
+          </Link>
+          <Link href="/messages">
+            <a className={styles.link}>Messages</a>
+          </Link>
+          <Tooltip title="Sign Out" arrow>
+            <button onClick={() => signOut(auth)}>
+              <ExitToAppIcon />
+            </button>
+          </Tooltip>
+        </>
+      }
     </div>
   );
 }
