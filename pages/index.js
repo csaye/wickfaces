a31fada0a5bec07b438f9d4ff0467fcf1b3887c8
Cooper Saye <coopersaye@gmail.com>
2021-11-15 08:16:04 -0800

    Clean up index page.

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,13 @@
-import CircularProgress from '@mui/material/CircularProgress';
-
 import Header from '../components/Header';
+import Register from '../components/Register';
+import LogIn from '../components/LogIn';
 
-import { useEffect, useState } from 'react';
-import {
-  getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink
-} from 'firebase/auth';
-import getError from '../util/getError';
+import { useState } from 'react';
 
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
-  const [email, setEmail] = useState('');
-  const [sent, setSent] = useState(false);
-  const [error, setError] = useState(undefined);
-
-  const auth = getAuth();
-
-  // check for email sign in
-  useEffect(() => {
-    // if sign in link opened
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-      // get email
-      let email = window.localStorage.getItem('emailForSignIn');
-      if (!email) {
-        email = window.prompt('Please provide your email for confirmation:');
-      }
-      // sign in with email link
-      signInWithEmailLink(auth, email, window.location.href)
-      .then(result => {
-        window.localStorage.removeItem('emailForSignIn');
-      })
-      .catch(e => setError(getError(e)));
-    }
-  }, [auth]);
-
-  // sends user registration email
-  async function register() {
-    // verify chadwick email
-    if (!email.endsWith('@chadwickschool.org')) {
-      setError('Please use your Chadwick email.');
-      return;
-    }
-    setSent(undefined);
-    const settings = {
-      url: 'http://localhost:3000',
-      handleCodeInApp: true
-    };
-    // send sign in link
-    sendSignInLinkToEmail(auth, email, settings)
-    .then(() => {
-      setSent(true);
-      window.localStorage.setItem('emailForSignIn', email);
-    })
-    .catch(e => setError(getError(e)));
-  }
+  const [register, setRegister] = useState(false);
 
   return (
     <div>
@@ -66,6 +19,9 @@ export default function Index() {
             <p>Connect with classmates and stay in touch after graduation.</p>
           </div>
           {
+            register ?
+            <Register setRegister={setRegister} /> :
+            <LogIn setRegister={setRegister} />
           }
         </div>
       </div>
