da26eab691b3c192c0e425a51369a1d7f10ab1b2
Cooper Saye <coopersaye@gmail.com>
2021-11-15 08:18:33 -0800

    Retrieve user data.

--- pages/[username].js
+++ pages/[username].js
@@ -1,6 +1,41 @@
+import Image from 'next/image';
+import Header from '../components/Header';
+
+import { getAuth } from 'firebase/auth';
+import {
+  getFirestore, collection, query, where, getDocs, doc, updateDoc
+} from 'firebase/firestore';
+import {
+  getStorage, ref, uploadBytes, getDownloadURL
+} from 'firebase/storage';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 import styles from '../styles/pages/User.module.css';
 
 export default function User() {
+  const router = useRouter();
+  const { username } = router.query;
+
+  // gets user data from firebase
+  async function getUserData() {
+    // query user
+    const userQuery = query(usersRef, where('username', '==', username));
+    const docs = await getDocs(userQuery);
+    // set user data
+    if (!docs.docs.length) setUserData(null);
+    else setUserData(docs.docs[0].data());
+  }
+
+  // get user data on start
+  useEffect(() => {
+    if (username) getUserData();
+  }, [username]);
+
+  // return if invalid data
+  if (userData === undefined) return <p>Loading...</p>;
+  if (!userData) return <p>{username} not found</p>;
+
   return (
     <div className={styles.container}>
       <Header />
