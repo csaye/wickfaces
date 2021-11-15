14bb7b64340190e077fe470a602a1a80898171a7
Cooper Saye <coopersaye@gmail.com>
2021-11-15 08:12:53 -0800

    Implement user registration.

--- components/Register.js
+++ components/Register.js
@@ -1,6 +1,53 @@
+import Router from 'next/router';
+import Alert from '@mui/material/Alert';
+
+import { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
+import getError from '../util/getError';
+
 import styles from '../styles/components/Register.module.css';
 
 export default function Register(props) {
+  const { setRegister } = props;
+
+  const auth = getAuth();
+  const db = getFirestore();
+
+  const usersRef = collection(db, 'users');
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // sends user registration email
+  async function register() {
+    setError('');
+    // verify email
+    if (!email.endsWith('@chadwickschool.org')) {
+      setError('Please use your Chadwick email.');
+      return;
+    }
+    // create user
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+      setError(getError(e));
+      return;
+    }
+    // create user doc on success
+    const uid = auth.currentUser.uid;
+    const userRef = doc(usersRef, uid);
+    const username = email.split('@')[0];
+    const yearStr = username.substring(username.length - 2);
+    const year = parseInt(yearStr);
+    const joined = new Date().getTime();
+    setDoc(userRef, { firstName, lastName, username, year, joined });
+    Router.push(`/${username}`);
+  }
+
   return (
     <div className={styles.container}>
       <form onSubmit={e => {
