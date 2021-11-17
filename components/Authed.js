72ae6f4ffd3d68ece58274e32f035748fea7d597
Cooper Saye <coopersaye@gmail.com>
2021-11-16 21:34:53 -0800

    Style Authed component.

--- components/Authed.js
+++ components/Authed.js
@@ -1,3 +1,7 @@
+import PeopleIcon from '@mui/icons-material/People';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ChatIcon from '@mui/icons-material/Chat';
 import Link from 'next/link';
 
 import styles from '../styles/components/Authed.module.css';
@@ -10,18 +14,30 @@ export default function Authed(props) {
       <h1>Welcome to Wickfaces!</h1>
       <p><i>To get started:</i></p>
       <div className={styles.links}>
-        <Link href="/users">
-          <a>Explore users</a>
-        </Link>
-        <Link href={`/${currUser?.username}`}>
-          <a>View your profile</a>
-        </Link>
-        <Link href="/board">
-          <a>Post to the board</a>
-        </Link>
-        <Link href="/messages">
-          <a>Send a message</a>
-        </Link>
+        <span>
+          <PeopleIcon />
+          <Link href="/users">
+            <a>Explore users</a>
+          </Link>
+        </span>
+        <span>
+          <AccountCircleIcon />
+          <Link href={`/${currUser?.username}`}>
+            <a>View your profile</a>
+          </Link>
+        </span>
+        <span>
+          <DashboardIcon />
+          <Link href="/board">
+            <a>Post to the board</a>
+          </Link>
+        </span>
+        <span>
+          <ChatIcon />
+          <Link href="/messages">
+            <a>Send a message</a>
+          </Link>
+        </span>
       </div>
     </div>
   );
