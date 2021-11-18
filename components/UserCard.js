9b5e4b73b6869932e20dd91dcbcdfc948cf94421
Cooper Saye <coopersaye@gmail.com>
2021-11-17 22:50:13 -0800

    Improve user card styling.

--- components/UserCard.js
+++ components/UserCard.js
@@ -2,11 +2,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Cover from './Cover';
 
+import styles from '../styles/components/UserCard.module.css';
+
 export default function UserCard(props) {
   const { username, firstName, lastName, year, cover } = props;
 
   return (
-    <div>
+    <div className={styles.container}>
       <Link href={`/${username}`}>
         <a>
           <p>{firstName} {lastName} &apos;{year}</p>
