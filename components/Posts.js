000f53774a8f5d9e5d1a79da77019b42cd75aef1
Cooper Saye <coopersaye@gmail.com>
2021-11-23 14:19:27 -0800

    Retrieve posts.

--- components/Posts.js
+++ components/Posts.js
@@ -1,8 +1,28 @@
+import Post from './Post';
+
+import { getFirestore, collection } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 import styles from '../styles/components/Posts.module.css';
 
-export default function Posts() {
+export default function Posts(props) {
+  const { uid } = props;
+
+  const db = getFirestore();
+
+  // listen for posts
+  const postsRef = collection(db, 'users', uid, 'posts');
+  const [posts] = useCollectionData(postsRef);
+
+  if (!posts) return <p>Loading...</p>;
+
   return (
     <div>
+      {
+        posts.map(post =>
+          <Post {...post} />
+        )
+      }
     </div>
   );
 }
