fdc071c6c8befc1838023df497534dc5c4d7b71d
Cooper Saye <coopersaye@gmail.com>
2021-11-23 14:51:11 -0800

    Create post form.

--- components/Posts.js
+++ components/Posts.js
@@ -1,17 +1,24 @@
 import Post from './Post';
 
+import { getAuth } from 'firebase/auth';
 import { getFirestore, collection } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 import styles from '../styles/components/Posts.module.css';
 
 export default function Posts(props) {
-  const { uid } = props;
+  const { poster } = props;
 
+  const [text, setText] = useState('');
+
+  const auth = getAuth();
   const db = getFirestore();
 
+  const uid = auth.currentUser.uid;
+
   // listen for posts
-  const postsRef = collection(db, 'users', uid, 'posts');
+  const postsRef = collection(db, 'users', poster, 'posts');
   const [posts] = useCollectionData(postsRef);
 
   if (!posts) return <p>Loading...</p>;
@@ -19,9 +26,24 @@ export default function Posts(props) {
   return (
     <div>
       {
+        !posts.length ?
+        <p>No posts yet</p> :
         posts.map(post =>
           <Post {...post} />
         )
+      }
+      {
+        poster === uid &&
+        <form onSubmit={e => {
+          e.preventDefault();
+        }}>
+          <textarea
+            value={text}
+            onChange={e => setText(e.target.value)}
+            required
+          />
+          <button>Create Post</button>
+        </form>
       }
     </div>
   );
