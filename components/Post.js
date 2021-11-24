761ff0d151eb798a76330bb5d99b9d6d804a4592
Cooper Saye <coopersaye@gmail.com>
2021-11-23 17:17:18 -0800

    Improve post styling.

--- components/Post.js
+++ components/Post.js
@@ -7,6 +7,7 @@ export default function Post(props) {
     <div className={styles.container}>
       <p>{text}</p>
       <span>{new Date(date).toLocaleDateString()}</span>
+      <div className={styles.arrow} />
     </div>
   );
 }
