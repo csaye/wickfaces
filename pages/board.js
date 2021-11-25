920f1eb98de7d6201e78214b43a3aa85cd4047d5
Cooper Saye <coopersaye@gmail.com>
2021-11-24 16:59:14 -0800

    Route from board page.

--- pages/board.js
+++ pages/board.js
@@ -1,22 +1,32 @@
+import Router from 'next/router';
 import Post from '../components/Post';
 
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { getFirestore, collection } from 'firebase/firestore';
+import { useEffect } from 'react';
 
 import styles from '../styles/pages/Board.module.css';
 
-export default function Board() {
+export default function Board(props) {
+  const { currUser } = props;
+
   const db = getFirestore();
 
   // get posts
   const postsRef = collection(db, 'board');
   const [posts] = useCollectionData(postsRef);
 
+  // route home if not authed
+  useEffect(() => {
+    if (currUser === false) Router.replace('/');
+  }, [currUser]);
+
+  // load if no data yet
+  if (!currUser || !posts) return <p>Loading...</p>;
+
   return (
     <div>
       {
-        !posts ?
-        <p>Loading...</p> :
         !posts.length ?
         <p>No posts yet</p> :
         posts.map(post =>
