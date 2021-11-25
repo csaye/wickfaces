ee43cc821188b7465ba369e6d869cb094c64ee9e
Cooper Saye <coopersaye@gmail.com>
2021-11-24 16:56:24 -0800

    Retrieve board posts.

--- pages/board.js
+++ pages/board.js
@@ -1,8 +1,28 @@
+import Post from '../components/Post';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { getFirestore, collection } from 'firebase/firestore';
+
 import styles from '../styles/pages/Board.module.css';
 
 export default function Board() {
+  const db = getFirestore();
+
+  // get posts
+  const postsRef = collection(db, 'board');
+  const [posts] = useCollectionData(postsRef);
+
   return (
     <div>
+      {
+        !posts ?
+        <p>Loading...</p> :
+        !posts.length ?
+        <p>No posts yet</p> :
+        posts.map(post =>
+          <Post key={post.id} />
+        )
+      }
     </div>
   );
 }
