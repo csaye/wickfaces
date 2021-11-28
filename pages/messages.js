416d70657c03d2adbfca407db5d8876bd32bab3f
Cooper Saye <coopersaye@gmail.com>
2021-11-27 23:38:59 -0800

    Route from messages page.

--- pages/messages.js
+++ pages/messages.js
@@ -1,6 +1,20 @@
+import Router from 'next/router';
+
+import { useEffect } from 'react';
+
 import styles from '../styles/pages/Messages.module.css';
 
-export default function Messages() {
+export default function Messages(props) {
+  const { currUser } = props;
+
+  // route home if not authed
+  useEffect(() => {
+    if (currUser === false) Router.replace('/');
+  }, [currUser]);
+
+  // load if no current user
+  if (!currUser) return <p>Loading...</p>;
+
   return (
     <div>
     </div>
