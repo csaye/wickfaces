931f8974654e03e867d20175ddee6860294aae79
Cooper Saye <coopersaye@gmail.com>
2021-11-27 22:44:54 -0800

    Populate post modal.

--- components/PostModal.js
+++ components/PostModal.js
@@ -1,8 +1,16 @@
+import Modal from './Modal';
+
 import styles from '../styles/components/PostModal.module.css';
 
-export default function PostModal() {
+export default function PostModal(props) {
+  const { post, open, setOpen } = props;
+  const { text, date, uid } = post;
+
   return (
-    <div>
-    </div>
+    <Modal open={open} setOpen={setOpen}>
+      <span>{text}</span>
+      <br />
+      <span>{new Date(date).toLocaleDateString()}</span>
+    </Modal>
   );
 }
