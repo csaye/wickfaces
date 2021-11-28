1dd4bef13185a224ac56c2d9d08a52d25c3debf6
Cooper Saye <coopersaye@gmail.com>
2021-11-28 11:22:15 -0800

    Add new post modal to Posts.

--- components/Posts.js
+++ components/Posts.js
@@ -1,18 +1,22 @@
 import Loading from './Loading';
 import Post from './Post';
-import Modal from './Modal';
+import NewPostModal from './NewPostModal';
+import AddIcon from '@mui/icons-material/Add';
 
 import { getAuth } from 'firebase/auth';
 import { getFirestore, query, orderBy } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 import styles from '../styles/components/Posts.module.css';
 
 export default function Posts(props) {
-  const { postsRef } = props;
+  const { postsRef, allowPosts } = props;
 
   const auth = getAuth();
   const db = getFirestore();
+
+  const [modalOpen, setModalOpen] = useState(false);
 
   // listen for posts
   const postsQuery = query(postsRef, orderBy('date', 'desc'));
@@ -31,6 +35,17 @@ export default function Posts(props) {
           )
         }
       </div>
+      {
+        allowPosts &&
+        <button onClick={() => setModalOpen(true)}>
+          <AddIcon />
+        </button>
+      }
+      <NewPostModal
+        postsRef={postsRef}
+        open={modalOpen}
+        setOpen={setModalOpen}
+      />
     </div>
   );
 }
