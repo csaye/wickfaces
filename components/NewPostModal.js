2b137433b4d6af678f02fd030b20041eed546298
Cooper Saye <coopersaye@gmail.com>
2021-11-27 21:12:48 -0800

    Create new post form.

--- components/NewPostModal.js
+++ components/NewPostModal.js
@@ -1,8 +1,24 @@
+import Modal from './Modal';
+
 import styles from '../styles/components/NewPostModal.module.css';
 
-export default function NewPostModal() {
+export default function NewPostModal(props) {
+  const { open, setOpen } = props;
+
   return (
-    <div>
-    </div>
+    <Modal open={open} setOpen={setOpen}>
+      <h1>New Post</h1>
+      <form onSubmit={e => {
+        e.preventDefault();
+        createPost();
+      }}>
+        <textarea
+          value={text}
+          onChange={e => setText(e.target.value)}
+          required
+        />
+        <button>Create Post</button>
+      </form>
+    </Modal>
   );
 }
