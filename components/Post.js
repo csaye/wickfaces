98a77921782bbbbcbef84cb47354dfa7f198df09
Cooper Saye <coopersaye@gmail.com>
2021-11-27 22:43:39 -0800

    Integrate post modal.

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,24 @@
+import PostModal from './PostModal';
+
+import { useState } from 'react';
+
 import styles from '../styles/components/Post.module.css';
 
 export default function Post(props) {
   const { text, date } = props;
 
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <p>{text}</p>
-      <span>{new Date(date).toLocaleDateString()}</span>
-    </div>
+    <>
+      <div
+        onClick={() => setModalOpen(true)}
+        className={styles.container}
+      >
+        <p>{text}</p>
+        <span>{new Date(date).toLocaleDateString()}</span>
+      </div>
+      <PostModal post={props} open={modalOpen} setOpen={setModalOpen} />
+    </>
   );
 }
