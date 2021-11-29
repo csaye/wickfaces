5284167b600209c3e64f4d82028c34d231d24403
Cooper Saye <coopersaye@gmail.com>
2021-11-29 08:24:21 -0800

    Improve authed card styling.

--- components/Authed.js
+++ components/Authed.js
@@ -12,32 +12,32 @@ export default function Authed(props) {
   return (
     <div className={styles.container}>
       <h1>Welcome to Wickfaces!</h1>
-      <p><i>To get started:</i></p>
+      <p>To get started:</p>
       <div className={styles.links}>
-        <span>
-          <PeopleIcon />
-          <Link href="/users">
-            <a>Explore users</a>
-          </Link>
-        </span>
-        <span>
-          <AccountCircleIcon />
-          <Link href={`/${currUser?.username}`}>
-            <a>View your profile</a>
-          </Link>
-        </span>
-        <span>
-          <DashboardIcon />
-          <Link href="/board">
-            <a>Post to the board</a>
-          </Link>
-        </span>
-        <span>
-          <ChatIcon />
-          <Link href="/messages">
-            <a>Send a message</a>
-          </Link>
-        </span>
+        <Link href="/users">
+          <a>
+            <PeopleIcon />
+            Explore users
+          </a>
+        </Link>
+        <Link href={`/${currUser?.username}`}>
+          <a>
+            <AccountCircleIcon />
+            View your profile
+          </a>
+        </Link>
+        <Link href="/board">
+          <a>
+            <DashboardIcon />
+            Post to the board
+          </a>
+        </Link>
+        <Link href="/messages">
+          <a>
+            <ChatIcon />
+            Send a message
+          </a>
+        </Link>
       </div>
     </div>
   );
