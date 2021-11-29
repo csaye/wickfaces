f44c70c8bdda897d2623a68e83985f4bc8dbc55d
Cooper Saye <coopersaye@gmail.com>
2021-11-28 18:06:46 -0800

    Clean up post modal.

--- components/PostModal.js
+++ components/PostModal.js
@@ -3,15 +3,11 @@ import Modal from './Modal';
 import styles from '../styles/components/PostModal.module.css';
 
 export default function PostModal(props) {
-  const { post, open, setOpen } = props;
-  const { text, date, uid } = post;
+  const { Content, open, setOpen } = props;
 
   return (
     <Modal open={open} setOpen={setOpen}>
-      <div className={styles.post}>
-        <p>{text}</p>
-        <span>{new Date(date).toLocaleDateString()}</span>
-      </div>
+      <Content />
     </Modal>
   );
 }
