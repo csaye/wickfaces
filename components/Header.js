a748d0efbd6fc1a9e6030fa46a9e7d59fc885faf
Cooper Saye <coopersaye@gmail.com>
2021-11-30 20:44:45 -0800

    Implement profile photo updating.

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,8 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import UploadIcon from '@mui/icons-material/Upload';
 
 import { getAuth, signOut } from 'firebase/auth';
+import { getFirestore, collection, doc, updateDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { useRef } from 'react';
 
 import styles from '../styles/components/Header.module.css';
@@ -15,8 +17,25 @@ export default function Header(props) {
   const { currUser } = props;
 
   const auth = getAuth();
-
+  const db = getFirestore();
+  const storage = getStorage();
   const fileRef = useRef();
+
+  const uid = currUser?.uid;
+  const usersRef = collection(db, 'users');
+
+  // uploads given profile to firebase
+  async function uploadProfile(image) {
+    if (!image) return;
+    // upload image
+    const filePath = `profiles/${uid}`;
+    const fileRef = ref(storage, filePath);
+    await uploadBytes(fileRef, image);
+    // update image
+    const url = await getDownloadURL(fileRef);
+    const userRef = doc(usersRef, uid);
+    await updateDoc(userRef, { profile: url });
+  }
 
   return (
     <div className={styles.container}>
@@ -55,11 +74,17 @@ export default function Header(props) {
           <SpeedDial
             ariaLabel="SpeedDial"
             direction="down"
+            sx={{
+              '& > button': {
+                background: `center center / cover url(${
+                  currUser.profile ?? '/img/blank/profile.png'
+                })`
+              }
+            }}
           >
             <SpeedDialAction
               onClick={() => signOut(auth)}
               icon={<ExitToAppIcon />}
-              name="Log Out"
               tooltipTitle="Log Out"
             />
             <SpeedDialAction
@@ -73,6 +98,7 @@ export default function Header(props) {
             type="file"
             accept="image/*"
             hidden={true}
+            onChange={e => uploadProfile(e.target.files[0])}
           />
         </div>
       }
