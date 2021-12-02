fcbef370c1addc75dee2e417419cbc44b0440607
Cooper Saye <coopersaye@gmail.com>
2021-12-01 22:52:47 -0800

    Listen for comments in post modal.

--- components/PostModal.js
+++ components/PostModal.js
@@ -1,6 +1,11 @@
+import Loading from './Loading';
+import Comment from './Comment';
 import Modal from './Modal';
 
-import { getFirestore, deleteDoc } from 'firebase/firestore';
+import {
+  getFirestore, deleteDoc, collection, query, orderBy
+} from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import styles from '../styles/components/PostModal.module.css';
 
@@ -8,6 +13,11 @@ export default function PostModal(props) {
   const { Content, open, setOpen, postRef } = props;
 
   const db = getFirestore();
+
+  // listen for comments
+  const commentsRef = collection(postRef, 'comments');
+  const commentsQuery = query(commentsRef, orderBy('date', 'desc'));
+  const [comments] = useCollectionData(commentsQuery, { idField: 'id' });
 
   // deletes post in firebase
   async function deletePost() {
@@ -21,6 +31,13 @@ export default function PostModal(props) {
       <button onClick={deletePost}>
         Delete Post
       </button>
+      {
+        !comments ?
+        <Loading /> :
+        comments.map(comment =>
+          <Comment comment={comment} key={comment.id} />
+        )
+      }
     </Modal>
   );
 }
