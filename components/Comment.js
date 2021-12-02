40b6ce1ee062762b54bfea1d8fd871ec8cf86395
Cooper Saye <coopersaye@gmail.com>
2021-12-01 23:04:36 -0800

    Improve comment styling.

--- components/Comment.js
+++ components/Comment.js
@@ -1,11 +1,16 @@
+import Link from 'next/link';
+
 import styles from '../styles/components/Comment.module.css';
 
 export default function Comment(props) {
   const { comment } = props;
-  const { text, date } = comment;
+  const { text, date, profile, uid, likes, name, username } = comment;
 
   return (
-    <div>
+    <div className={styles.container}>
+      <Link href={`/${username}`}>
+        <a>{name}</a>
+      </Link>
       <p>{text}</p>
       <span>
         {new Date(date).toLocaleDateString()}
