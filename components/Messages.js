6e8f57cc61e2f6d3332310b3936465ef48b852c9
Cooper Saye <coopersaye@gmail.com>
2021-12-03 23:46:53 -0800

    Listen for messages.

--- components/Messages.js
+++ components/Messages.js
@@ -1,8 +1,21 @@
+import Loading from './Loading';
 import Message from './Message';
+
+import { query, orderBy } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import styles from '../styles/components/Messages.module.css';
 
-export default function Messages() {
+export default function Messages(props) {
+  const { messagesRef } = props;
+
+  // listen for posts
+  const messagesQuery = query(messagesRef, orderBy('date', 'desc'));
+  const [messages] = useCollectionData(messagesQuery, { idField: 'id' });
+
+  // return if loading
+  if (!messages) return <Loading />;
+
   return (
     <div>
       {
