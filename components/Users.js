bdbda3b10186cfb1e9fda1ced92b9ba5b31808dc
Cooper Saye <coopersaye@gmail.com>
2021-12-03 23:51:11 -0800

    Retrieve users from firebase.

--- components/Users.js
+++ components/Users.js
@@ -1,6 +1,32 @@
+import Loading from '../components/Loading';
+
+import { useEffect, useState } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
 import styles from '../styles/components/Users.module.css';
 
-export default function Users() {
+export default function Users(props) {
+  const { currUser } = props;
+
+  const db = getFirestore();
+
+  const [usersData, setUsersData] = useState(undefined);
+
+  // retrieves users from firebase
+  async function getUsers() {
+    const usersRef = collection(db, 'users');
+    const users = await getDocs(usersRef);
+    setUsersData(users.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+  }
+
+  // get users on start
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // return if loading
+  if (!usersData) return <Loading />;
+
   return (
     <div>
       {
