ec26212a142a4e3e93a9604b3edabe5f0c95484e
Cooper Saye <coopersaye@gmail.com>
2021-12-08 10:57:37 -0800

    Create comment heart button.

--- components/Comment.js
+++ components/Comment.js
@@ -1,15 +1,27 @@
 import Link from 'next/link';
 import Profile from './Profile';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import getAgoText from '../util/getAgoText';
-import { deleteDoc } from 'firebase/firestore';
+import {
+  deleteDoc, updateDoc, arrayRemove, arrayUnion
+} from 'firebase/firestore';
 
 import styles from '../styles/components/Comment.module.css';
 
 export default function Comment(props) {
   const { comment, commentRef, currUser } = props;
   const { text, date, profile, uid, likes, name, username } = comment;
+
+  // toggles like on post
+  async function toggleLike() {
+    await updateDoc(commentRef, {
+      likes:
+        (likes.includes(currUser.uid) ? arrayRemove : arrayUnion)(currUser.uid)
+    });
+  }
 
   // deletes comment in firebase
   async function deleteComment() {
@@ -31,6 +43,14 @@ export default function Comment(props) {
         <span>{getAgoText(new Date(date))}</span>
         <p>{text}</p>
       </div>
+      <span>{likes.length}</span>
+      <button onClick={toggleLike}>
+        {
+          likes.includes(currUser.uid) ?
+          <FavoriteIcon /> :
+          <FavoriteBorderIcon />
+        }
+      </button>
       {
         currUser.uid === uid &&
         <button
