33ce7fa727d23690569d16e2baa2529fd48a7386
Cooper Saye <coopersaye@gmail.com>
2021-12-08 15:43:43 -0800

    Implement dynamic images.

--- components/DynamicImage.js
+++ components/DynamicImage.js
@@ -1,13 +1,30 @@
+import Loading from './Loading';
 import Image from 'next/image';
 
 import styles from '../styles/components/DynamicImage.module.css';
 
 export default function DynamicImage(props) {
-  const { src } = props;
+  const { src, width, height, placeholder } = props;
 
   return (
-    <div>
-      <Image src={src} />
+    <div
+      className={styles.container}
+      style={{
+        width, height,
+        backgroundImage: placeholder ? `url(${placeholder})` : null
+      }}
+    >
+      {
+        src === undefined ?
+        <Loading /> :
+        <Image
+          placeholder="blur"
+          blurDataURL={src}
+          src={src}
+          layout="fill"
+          objectFit="cover"
+        />
+      }
     </div>
   );
 }
