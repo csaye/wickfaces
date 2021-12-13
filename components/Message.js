8a402a5169fb13ada9b4c9cba79c3c0b3500255d
Cooper Saye <coopersaye@gmail.com>
2021-12-12 21:57:11 -0800

    Create message head.

--- components/Message.js
+++ components/Message.js
@@ -3,11 +3,18 @@ import getAgoText from '../util/getAgoText';
 import styles from '../styles/components/Message.module.css';
 
 export default function Message(props) {
-  const { text, date } = props;
+  const { text, date, name } = props;
 
   return (
     <div className={styles.container}>
-      <span>{getAgoText(new Date(date))}</span>
+      <p className={styles.head}>
+        <span className={styles.name}>
+          {name}
+        </span>
+        <span className={styles.date}>
+          {getAgoText(new Date(date))}
+        </span>
+      </p>
       <p>{text}</p>
     </div>
   );
