387b6c421980e54f90050c6222184707f39924e9
Cooper Saye <coopersaye@gmail.com>
2021-12-12 21:58:40 -0800

    Link message to profile.

--- components/Message.js
+++ components/Message.js
@@ -1,16 +1,20 @@
+import Link from 'next/link';
+
 import getAgoText from '../util/getAgoText';
 
 import styles from '../styles/components/Message.module.css';
 
 export default function Message(props) {
-  const { text, date, name } = props;
+  const { text, date, name, username } = props;
 
   return (
     <div className={styles.container}>
-      <p className={styles.head}>
-        <span className={styles.name}>
-          {name}
-        </span>
+      <p>
+        <Link href={`/${username}`}>
+          <a className={styles.name}>
+            {name}
+          </a>
+        </Link>
         <span className={styles.date}>
           {getAgoText(new Date(date))}
         </span>
