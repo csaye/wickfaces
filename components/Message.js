57dd326dba3730e035d169cbe1ad547b93b5eac6
Cooper Saye <coopersaye@gmail.com>
2021-12-12 22:40:36 -0800

    Make message header conditional.

--- components/Message.js
+++ components/Message.js
@@ -5,20 +5,24 @@ import getAgoText from '../util/getAgoText';
 import styles from '../styles/components/Message.module.css';
 
 export default function Message(props) {
-  const { text, date, name, username } = props;
+  const { message, showHeader } = props;
+  const { text, date, name, username } = message;
 
   return (
     <div className={styles.container}>
-      <p>
-        <Link href={`/${username}`}>
-          <a className={styles.name}>
-            {name}
-          </a>
-        </Link>
-        <span className={styles.date}>
-          {getAgoText(new Date(date))}
-        </span>
-      </p>
+      {
+        showHeader &&
+        <p>
+          <Link href={`/${username}`}>
+            <a className={styles.name}>
+              {name}
+            </a>
+          </Link>
+          <span className={styles.date}>
+            {getAgoText(new Date(date))}
+          </span>
+        </p>
+      }
       <p>{text}</p>
     </div>
   );
